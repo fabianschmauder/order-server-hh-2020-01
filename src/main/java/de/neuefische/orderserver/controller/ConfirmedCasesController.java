bc84cab89c2c4bd3c7f2ff1fb37a7b2a4f3227ea
Fabian Schmauder <fabian.schmauder@gmail.com>
2020-10-07 10:41:38 +0200

    Remove char

--- src/main/java/de/neuefische/orderserver/controller/ConfirmedCasesController.java
+++ src/main/java/de/neuefische/orderserver/controller/ConfirmedCasesController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("covid/confirmed")^
+@RequestMapping("covid/confirmed")
 public class ConfirmedCasesController {
 
     private final CovidService covidService;
